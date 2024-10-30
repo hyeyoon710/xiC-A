6f691bc50c58efdb906f802e4d1c44475751e857
HY <coco5303@naver.com>
2024-10-30 14:43:43 +0900

    xi

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
-
-// $('nav #gnb>li>a').hover(function(){
-//   $(header).addclass('on')
-// });
-// $('nav #gnb>li>a').mouseout(function(){
-//   $('header.on').removeClass('on')
-// });
-
-
-// $('#gnb>li').hover(function(){
-//   $(this).children('.dep2').stop().slideDown()
-//   $('header').addClass('on')
-//   }, function(){
-//   $('#gnb>li .dep2').stop().slideUp();
-//   $('header').removeClass('on')
-// })
-
 $('#gnb>li').hover(function(){
   $('.dep2').stop().fadeIn(500, "linear")
   $('.gnb_bg').stop().fadeIn(500, "linear")
   $('header').addClass('on')
+  $('.btn_util img').attr('src', '../image/icon_search_blue.svg');
   }, function(){
   $('.dep2').stop().fadeOut(500, "linear")
   $('.gnb_bg').stop().fadeOut(500, "linear")
   $('header').removeClass('on')
+  $('.btn_util img').attr('src', 'image/icon_search_white.svg');
+})
+
+$('.family_wrap button').click(function(){
+  $(this).toggleClass('open');
+  $('.family_wrap ul').toggle();
 })
 
 const swiper = new Swiper('.main_swiper',{
