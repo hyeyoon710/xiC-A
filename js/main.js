89276949f2e87e515aa207d475e304b986107b21
HY <coco5303@naver.com>
2024-10-31 11:37:59 +0900

    xi

--- js/main.js
+++ js/main.js
@@ -15,28 +15,47 @@ $('.family_wrap button').click(function(){
   $('.family_wrap ul').toggle();
 })
 
-const swiper = new Swiper('.main_swiper',{
-  autoplay: true,
+const swiper = new Swiper('.main_swiper', {
+  effect: 'fade',
+  speed: 2000,
   loop: true,
 
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false, // 사용자가 클릭해도 자동 재생 유지
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   }
-})
+});
 
-window.addEventListener('load', () => {
-  gsap.registerPlugin(ScrollTrigger);
 
-  gsap.to('.img_wrap img', {
-    transformOrigin: 'center center',
-    scrollTrigger : {
-      trigger : 'img',
-      start : 'top top',
-      // end : 'bottom top',
-      pin : true,
-      scrub : true,
-      markers: true,
-    }
-  })
-})
+$(window).on('scroll', function() {
+  var imgTop = $('img').offset().top; // .visual의 top 위치
+  var scrollTop = $(window).scrollTop(); // 현재 스크롤 위치
+  var windowHeight = $(window).height(); // 윈도우 창 높이
+
+  // 스크롤 위치가 .visual의 top에 닿았을 때
+  if (scrollTop + windowHeight >= imgTop) {
+    setTimeout(function() {
+      $('.img_bg_left, .img_bg_right').css('width', '0');
+    } );
+  }
+});
+
+// window.addEventListener('load', () => {
+//   gsap.registerPlugin(ScrollTrigger);
+
+//   gsap.to('.img_wrap img', {
+//     transformOrigin: 'center center',
+//     scrollTrigger : {
+//       trigger : 'img',
+//       start : 'top top',
+//       // end : 'bottom top',
+//       pin : true,
+//       scrub : true,
+//       markers: true,
+//     }
+//   })
+// })
